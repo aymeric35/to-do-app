bd11870fdde3ddc2e7f5e03d5192bd7050bc0c92
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-24 13:14:55 +0200

    Add theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#6366F1',
+                secondary: '#312E81',
+                accent: '#FAFAFA',
+                error: '#DC2626',
+            },
+            dark: {
+                primary: '#171717',
+                secondary: '#b0bec5',
+                accent: '#8c9eff',
+                error: '#DC2626',
+            }
+        },
+    },
 });
