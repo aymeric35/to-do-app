3a9efd32957e17e501a189f2922dc1b9a4c97458
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-27 13:47:42 +0200

    Add routes to menu items

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-navigation-drawer color="secondary" permanent expand-on-hover app>
       <v-list nav dense>
-        <v-list-item link>
+        <v-list-item to="/" link>
           <v-list-item-icon>
             <v-icon color="primary">mdi-inbox</v-icon>
           </v-list-item-icon>
@@ -10,7 +10,7 @@
             >Inbox</v-list-item-title
           >
         </v-list-item>
-        <v-list-item link>
+        <v-list-item to="/list" link>
           <v-list-item-icon>
             <v-icon color="primary">mdi-format-list-bulleted</v-icon>
           </v-list-item-icon>
@@ -18,7 +18,7 @@
             >My lists</v-list-item-title
           >
         </v-list-item>
-        <v-list-item link>
+        <v-list-item to="/settings" link>
           <v-list-item-icon>
             <v-icon color="primary">mdi-cog</v-icon>
           </v-list-item-icon>
@@ -42,34 +42,7 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      dialog: false,
-      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-        .toISOString()
-        .substr(0, 10),
-      menu: false,
-      desserts: [
-        {
-          completed: false,
-          name: "Go smite pd",
-          description: "Jouer à Smite (ps: je déconne, jeune de merde)",
-        },
-        {
-          completed: false,
-          name: "jouer à Tales of Arise le bge",
-          description: "nofake",
-        },
-        {
-          completed: false,
-          name: "Racheter CIG et sortir Star Citizen",
-          description: "Trouver 2 milliards avant",
-        },
-      ],
-    };
-  },
-};
+export default {};
 </script>
 
 <style lang="scss">
