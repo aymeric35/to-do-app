0a52c2d8aee42cef7f5d315115be40427c3b985d
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-27 15:18:36 +0200

    Add Navigation page component

---
+++ src/components/TheNavigationPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-list-item v-bind="$attrs" link>
+    <v-list-item-icon>
+      <v-icon color="primary"> {{ icon }} </v-icon>
+    </v-list-item-icon>
+    <v-list-item-title class="grey--text text--lighten-5">
+      {{ title }}</v-list-item-title
+    >
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
