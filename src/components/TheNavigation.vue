31e753c02f1638eb935f756265c9fd98a8cf6fbd
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-27 15:17:44 +0200

    Import TheNavigationPage.vue component

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,30 +1,31 @@
 <template>
   <v-navigation-drawer color="secondary" permanent expand-on-hover app>
     <v-list nav dense>
-      <v-list-item to="/" link>
-        <v-list-item-icon>
-          <v-icon color="primary">mdi-inbox</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title class="grey--text text--lighten-5"
-          >Inbox</v-list-item-title
-        >
-      </v-list-item>
-      <v-list-item to="/list" link>
-        <v-list-item-icon>
-          <v-icon color="primary">mdi-format-list-bulleted</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title class="grey--text text--lighten-5"
-          >My lists</v-list-item-title
-        >
-      </v-list-item>
-      <v-list-item to="/settings" link>
-        <v-list-item-icon>
-          <v-icon color="primary">mdi-cog</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title class="grey--text text--lighten-5"
-          >Settings</v-list-item-title
-        >
-      </v-list-item>
+      <TheNavigationPage
+        to="/"
+        icon="mdi-inbox"
+        title="Inbox"
+      ></TheNavigationPage>
+      <TheNavigationPage
+        to="/list"
+        icon="mdi-format-list-bulleted"
+        title="My lists"
+      ></TheNavigationPage>
+      <TheNavigationPage
+        to="/settings"
+        icon="mdi-cog"
+        title="Settings"
+      ></TheNavigationPage>
     </v-list>
   </v-navigation-drawer>
 </template>
+
+<script>
+import TheNavigationPage from "../components/TheNavigationPage";
+
+export default {
+  components: {
+    TheNavigationPage,
+  },
+};
+</script>
