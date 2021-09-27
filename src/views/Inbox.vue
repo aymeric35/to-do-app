04cd73f7e897855c5d4eed8904f6a1a57e9c1bbe
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-27 21:44:31 +0200

    Add form inputs rules

--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container>
       <v-row class="d-flex justify-end py-6 pr-2">
-        <v-dialog v-model="dialog" max-width="600px">
+        <v-dialog v-model="dialog" max-width="600px" persistent>
           <template v-slot:activator="{ on, attrs }">
             <v-btn elevation="2" color="primary" fab v-bind="attrs" v-on="on">
               <v-icon>mdi-plus</v-icon>
@@ -15,71 +15,101 @@
             </v-card-title>
             <v-card-text>
               <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6">
-                    <v-text-field label="Task name" required></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6">
-                    <v-text-field label="Task priority" required></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="8">
-                    <v-textarea
-                      name="input-7-1"
-                      rows="1"
-                      label="Task description"
-                      auto-grow
-                    ></v-textarea>
-                  </v-col>
-                  <v-col cols="12" sm="4">
-                    <v-menu
-                      ref="menu"
-                      v-model="menu"
-                      :close-on-content-click="false"
-                      :return-value.sync="date"
-                      transition="scale-transition"
-                      offset-y
-                      min-width="auto"
-                    >
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-text-field
-                          v-model="date"
-                          label="Due date"
-                          prepend-icon="mdi-calendar"
-                          readonly
-                          v-bind="attrs"
-                          v-on="on"
-                        ></v-text-field>
-                      </template>
-                      <v-date-picker
-                        v-model="date"
-                        color="primary"
-                        no-title
-                        scrollable
+                <v-form ref="form" v-model="valid" lazy-validation>
+                  <v-row>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        v-model="name"
+                        label="Task name*"
+                        :counter="30"
+                        :rules="rule.name"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        v-model="priority"
+                        label="Task priority*"
+                        type="number"
+                        :rules="rule.priority"
+                        min="0"
+                        max="9999"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="8">
+                      <v-textarea
+                        v-model="description"
+                        name="input-7-1"
+                        rows="1"
+                        label="Task description*"
+                        :counter="200"
+                        :rules="rule.description"
+                        auto-grow
+                      ></v-textarea>
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <v-menu
+                        ref="menu"
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :return-value.sync="date"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
                       >
-                        <v-spacer></v-spacer>
-                        <v-btn text color="primary" @click="menu = false">
-                          Cancel
-                        </v-btn>
-                        <v-btn
-                          text
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="date"
+                            label="Due date"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="date"
                           color="primary"
-                          @click="$refs.menu.save(date)"
+                          :min="new Date((Date.now())).toISOString()"
+                          no-title
+                          scrollable
                         >
-                          OK
-                        </v-btn>
-                      </v-date-picker>
-                    </v-menu>
-                  </v-col>
-                </v-row>
+                          <v-spacer></v-spacer>
+                          <v-btn text color="primary" @click="menu = false">
+                            Cancel
+                          </v-btn>
+                          <v-btn
+                            text
+                            color="primary"
+                            @click="$refs.menu.save(date)"
+                          >
+                            OK
+                          </v-btn>
+                        </v-date-picker>
+                      </v-menu>
+                    </v-col>
+                  </v-row>
+                </v-form>
               </v-container>
               <small>*indicates required field</small>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="dialog = false">
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="[closeDialog(), reset()]"
+              >
                 Close
               </v-btn>
-              <v-btn color="blue darken-1" text @click="dialog = false">
+              <v-btn
+                :disabled="!valid"
+                color="blue darken-1"
+                type="submit"
+                text
+                @click="[validate()]"
+              >
                 Save
               </v-btn>
             </v-card-actions>
@@ -119,6 +149,29 @@ export default {
         .toISOString()
         .substr(0, 10),
       menu: false,
+      valid: true,
+      name: "",
+      priority: 1,
+      description: "",
+      rule: {
+        name: [
+          (v) => !!v || "Name is required",
+          (v) =>
+            (v && v.length <= 30) || "Name must be less than 30 characters",
+        ],
+        priority: [
+          (v) => !!v || "Priority is required",
+          (v) =>
+            (v > 0 && v < 10000) ||
+            "Priority must be a number between 1 and 9999",
+        ],
+        description: [
+          (v) => !!v || "Description is required",
+          (v) =>
+            (v && v.length <= 200) ||
+            "Description must be less than 200 characters",
+        ],
+      },
       desserts: [
         {
           completed: false,
@@ -137,6 +190,19 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    closeDialog() {
+      this.dialog = false;
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.closeDialog();
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
   },
 };
 </script>
