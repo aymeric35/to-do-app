efa79941baefcdb08fcb33cd1e8ad749babc73a5
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-30 10:44:11 +0200

    Add dynamic routing for List

--- src/router/index.js
+++ src/router/index.js
@@ -11,20 +11,14 @@ const routes = [
     component: Inbox
   },
   {
-    path: '/list',
+    path: '/list/:list',
     name: 'List',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/List.vue')
+    component: () => import('../views/List.vue')
   },
   {
     path: '/settings',
     name: 'Settings',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Settings.vue')
+    component: () => import('../views/Settings.vue')
   },
 ]
 
