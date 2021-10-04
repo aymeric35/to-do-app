786389ab4b12c7363b429c6ebef7d624956e5315
Aymeric <aymeric.bertrand35@gmail.com>
2021-10-04 11:05:11 +0200

    Create 404.vue

---
+++ src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-container>
+      <v-row align="center" class="py-6">
+        <v-col
+          cols="12"
+          align="center"
+          class="text-center grey--text text--lighten-5 text-h4"
+        >
+          Page not found
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
