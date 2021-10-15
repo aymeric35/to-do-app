7db94c433bbf718aa90455bd816436e26e18ea50
Aymeric <aymeric.bertrand35@gmail.com>
2021-10-15 18:56:29 +0200

    Add dummy tasks

--- src/store.js
+++ src/store.js
@@ -1,4 +1,51 @@
+import { v4 as uuidv4 } from "uuid";
+
 export default {
-    tasks: [],
-    lists: []
+    tasks: [
+        {
+            uuid: uuidv4(),
+            page: 'test1',
+            completed: false,
+            name: 'Bonjour',
+            description: 'test1',
+            priority: 1,
+            date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+                .toISOString()
+                .substr(0, 10),
+        },
+        {
+            uuid: uuidv4(),
+            page: 'test1',
+            completed: false,
+            name: 'Hello',
+            description: 'test1',
+            priority: 1,
+            date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+                .toISOString()
+                .substr(0, 10),
+        },
+        {
+            uuid: uuidv4(),
+            page: 'test2',
+            completed: false,
+            name: 'Au revoir',
+            description: 'test2',
+            priority: 1,
+            date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+                .toISOString()
+                .substr(0, 10),
+        },
+        {
+            uuid: uuidv4(),
+            page: 'test2',
+            completed: false,
+            name: 'Goodbye',
+            description: 'test2',
+            priority: 1,
+            date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+                .toISOString()
+                .substr(0, 10),
+        },
+    ],
+    lists: ['test1', 'test2']
 }
