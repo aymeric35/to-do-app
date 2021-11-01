49547a55520ad9b828f8d7061ef0ee228778ce53
Aymeric <aymeric.bertrand35@gmail.com>
2021-11-01 18:13:22 +0100

    Add UPDATE_PRIORITY and update CHECK_TASK_COMPLETION name to DELETE_TASK

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
         completed: false,
         name: 'Task 0',
         description: 'placeholder',
-        priority: 1,
+        priority: 0,
         date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
           .toISOString()
           .substr(0, 10),
@@ -24,7 +24,7 @@ export default new Vuex.Store({
         completed: false,
         name: 'Task 1',
         description: 'placeholder',
-        priority: 2,
+        priority: 1,
         date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
           .toISOString()
           .substr(0, 10),
@@ -35,7 +35,7 @@ export default new Vuex.Store({
         completed: false,
         name: 'Task 2',
         description: 'placeholder',
-        priority: 3,
+        priority: 2,
         date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
           .toISOString()
           .substr(0, 10),
@@ -46,7 +46,7 @@ export default new Vuex.Store({
         completed: false,
         name: 'Task 3',
         description: 'placeholder',
-        priority: 4,
+        priority: 3,
         date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
           .toISOString()
           .substr(0, 10),
@@ -57,7 +57,7 @@ export default new Vuex.Store({
         completed: false,
         name: 'Task 4',
         description: 'placeholder',
-        priority: 5,
+        priority: 3,
         date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
           .toISOString()
           .substr(0, 10),
@@ -80,17 +80,19 @@ export default new Vuex.Store({
       state.tasks = filtered;
       this.commit('SAVE_STORE')
     },
-    CHECK_TASK_COMPLETION(state, { item, uuid }) {
-      if (item.completed === true) {
-        const index = state.tasks.findIndex(task => task.uuid === uuid)
-        state.tasks.splice(index, 1)
-        this.commit('SAVE_STORE')
-      }
+    UPDATE_PRIORITY(state, { currentTask, i }) {
+      const task = state.tasks.find(task => task.uuid === currentTask.uuid)
+      task.priority = i
+      this.commit('SAVE_STORE')
+    },
+    DELETE_TASK(state, uuid) {
+      const index = state.tasks.findIndex(task => task.uuid === uuid)
+      state.tasks.splice(index, 1)
+      this.commit('SAVE_STORE')
     },
     SAVE_STORE(state) {
       const parsedTasks = JSON.stringify(state.tasks);
       const parsedLists = JSON.stringify(state.lists);
-      console.log(parsedLists);
       localStorage.setItem('tasks', parsedTasks);
       localStorage.setItem('lists', parsedLists);
     },
